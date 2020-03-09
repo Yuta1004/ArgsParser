c80b6a33ba94e7c6e54808b7a45c8db87badb869
NakagamiYuta <guguru0014@gmail.com>
2020-03-09 20:34:49 +0900

    [add] Test.checkメソッド実装

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,13 @@
 class Test {
 
     public static void main(String[] args) {
-        System.out.println("called Test.class");
+    }
+
+    private static <T> void check(String msg, T a, T b) {
+        if(a.equals(b))
+            System.out.println(msg + ": OK");
+        else
+            System.out.println(msg + ": NG (" + a + " != " + b + ")");
     }
 
 }
